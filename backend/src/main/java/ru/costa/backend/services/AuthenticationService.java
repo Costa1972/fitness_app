6b3c157dc9173b8ff0956fa1bdc54519bedd6809
Constantin Vashchuk <costan1972@gmail.com>
2024-05-27 20:45:29 +0300

    created services

---
+++ backend/src/main/java/ru/costa/backend/services/AuthenticationService.java
@@ -0,0 +1,39 @@
+package ru.costa.backend.services;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.lang.NonNull;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
+@RequiredArgsConstructor
+public class AuthenticationService {
+    @Value("${errors.wrong_username_or_password}")
+    private String WRONG_USERNAME_OR_PASSWORD;
+    private final UserService userService;
+    private final JwtAuthenticationProvider jwtAuthenticationProvider;
+    private final AuthenticationManager authenticationManager;
+
+    public ResponseEntity<?> generateAccessToken(@NonNull AccessTokenRequest tokenRequest) {
+        Map<String, String> refreshStorage = new HashMap<>();
+        try {
+            authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(tokenRequest.getUsername(), tokenRequest.getPassword()));
+        } catch (BadCredentialsException badCredentialsException) {
+            return new ResponseEntity<>(new ApplicationError(HttpStatus.UNAUTHORIZED.value(), WRONG_USERNAME_OR_PASSWORD), HttpStatus.UNAUTHORIZED);
+        }
+        UserDetails userDetails = userService.loadUserByUsername(tokenRequest.getUsername());
+        String accessToken = jwtAuthenticationProvider.generateAccessToken(userDetails);
+        String refreshToken = jwtAuthenticationProvider.generateRefreshToken(userDetails);
+        refreshStorage.put("token", refreshToken);
+        return ResponseEntity.ok(new TokenResponse(accessToken, refreshToken));
+    }
+}
---
+++ backend/src/main/java/ru/costa/backend/services/UserService.java
@@ -0,0 +1,41 @@
+package ru.costa.backend.services;
+
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import ru.costa.backend.entities.User;
+import ru.costa.backend.repositories.UserRepository;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * @author Constantin Vashchuk
+ */
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class UserService implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException(username));
+        return new org.springframework.security.core.userdetails.User(
+                user.getUsername(),
+                user.getPassword(),
+                user.getRoles().stream().map(role ->
+                        new SimpleGrantedAuthority(role.getName())).collect(Collectors.toList())
+        );
+    }
+}
